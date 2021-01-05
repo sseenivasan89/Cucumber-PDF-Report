56ca467b8faac2eace71bdaa9998b8063859e3f5
unknown <sseenivasan89@gmail.com>
2021-01-05 12:51:40 +0530

    'first'

---
+++ src/main/java/Cucumber/PdfReport/App.java
@@ -0,0 +1,13 @@
+package Cucumber.PdfReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Steps/Hooks.java
@@ -0,0 +1,58 @@
+package Steps;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.cucumber.java.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Hooks {
+
+	public static WebDriver driver;
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.silentOutput", "true");
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("enable-automation");
+		//options.addArguments("--headless");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--dns-prefetch-disable");
+		options.addArguments("--disable-gpu");
+		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+		options.addArguments("enable-features=NetworkServiceInProcess");
+		options.addArguments("disable-features=NetworkService");
+		driver = new ChromeDriver(options);
+	}
+	
+	@After
+	public void tearDown(Scenario scenario) {
+
+		try {
+			String screenshotName = scenario.getName().replaceAll(" ", "_");
+			if (scenario.isFailed()) {
+				scenario.log("this is my failure message");
+				TakesScreenshot ts = (TakesScreenshot) driver;
+				byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
+				scenario.attach(screenshot, "image/png", screenshotName);
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		driver.close();
+	}
+
+}
